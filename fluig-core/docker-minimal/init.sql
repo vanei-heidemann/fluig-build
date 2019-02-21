5e3c9c7ea02e4ffd09f710fbae9aac99a721de15
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2019-02-21 17:57:13 -0300

    Removido fluig-communication

--- fluig-core/docker-minimal/init.sql
+++ fluig-core/docker-minimal/init.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE IF NOT EXISTS fluig CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE IF NOT EXISTS fluig_comm CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE IF NOT EXISTS fluig_i18n CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE IF NOT EXISTS fluig_discovery CHARACTER SET utf8 COLLATE utf8_general_ci;
 grant all on *.* to 'fluig'@'%' identified by 'OLyVu1gIwWmEr';
