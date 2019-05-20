f660b417cea46f20f492bea3005dfa51a079e0d6
Bruno Dix <brunodix@gmail.com>
2019-05-20 10:13:34 -0300

    Ajuste nos grants para funcionamento correto do read-replica.

--- fluig-core/docker-replica/init.sql
+++ fluig-core/docker-replica/init.sql
@@ -3,5 +3,4 @@ CREATE DATABASE IF NOT EXISTS fluig_i18n CHARACTER SET utf8 COLLATE utf8_general
 CREATE DATABASE IF NOT EXISTS fluig_discovery CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE IF NOT EXISTS fluig_tasks CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE IF NOT EXISTS boards CHARACTER SET utf8 COLLATE utf8_general_ci;
-grant all on *.* to 'fluig'@'%' identified by 'OLyVu1gIwWmEr';
 FLUSH PRIVILEGES;
